fee327041138b3c0815c19ca2ae0f99f70f90956
benedict-ux <benedictcarungcong@gmail.com>
2026-04-26 18:51:42 +0800

    search bar

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'str-system-v78';
+const CACHE_NAME = 'str-system-v84';
 const ASSETS = [
   '/index.html',
   'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js',
