92acacb0f0fcd03bfb4295f5b04c1f109c27b8a6
benedict-ux <benedictcarungcong@gmail.com>
2026-05-18 16:41:01 +0800

    fix the proper arrangement and print

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'str-system-v107';
+const CACHE_NAME = 'str-system-v109';
 const ASSETS = [
   '/index.html',
   'https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js',
